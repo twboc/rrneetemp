66f84d04dbc9ef87555ba9181816c8df098f4bee
twboc <hubert.sobkow@gmail.com>
2023-05-29 13:41:42 +0200

    cleanup

--- app/container/organisationEditor/organisationEditor.tsx
+++ app/container/organisationEditor/organisationEditor.tsx
@@ -2,42 +2,8 @@ import React, {useState, FC} from 'react'
 import InputLabel from '../../component/inputLabel/inputLabel'
 import ErrorLabel from '../../component/errorLabel/errorLabel'
 import {onChange} from '../../util/util'
-import resource from '../../resource/resource'
-import {organisation} from '../../state/state.actions'
 import OrganisationEditorProps from './organisationEditor.type'
 import {changeName} from './organisationEditor.action'
-
-// const changeName =
-//   (isChangingName, setIsChangingName, setError, props, name, setName) =>
-//   async () => {
-//     try {
-//       if (isChangingName) return
-//       setIsChangingName(true)
-//       console.log('Start')
-//       setError(false)
-//       const result = await resource.api.organisation.changeName({
-//         organisation_id: props.organisation.organisation_id,
-//         name,
-//       })
-
-//       if (!result.success) {
-//         setError(true)
-//         return setTimeout(() => {
-//           setError(false)
-//         }, 3000)
-//       }
-
-//       organisation.setName({
-//         ...props.organisation,
-//         name: result.data.name,
-//       })
-
-//       // In case user changes the input before the response from server
-//       setName(result.data.name)
-//     } finally {
-//       setIsChangingName(false)
-//     }
-//   }
 
 const OrganisationEditor: FC<OrganisationEditorProps> = props => {
   const [name, setName] = useState(props.organisation.name)
