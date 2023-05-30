a07e32b122464aa28755f69f097aa8716555993d
twboc <hubert.sobkow@gmail.com>
2023-05-30 21:10:27 +0200

    simplify name

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,17 +1,17 @@
 import { Request, Response, NextFunction } from 'express'
 import respond from '../respond/respond'
-import authorizationModule from '../module/authorization/authorization'
+import authorization from '../module/authorization/authorization'
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
-  const authorization = authorizationModule.exists(req)
-  if (authorization == null) return res.sendStatus(401)
-  const isValid = await authorizationModule.validate(authorization)
+  const bearer = authorization.exists(req)
+  if (bearer == null) return res.sendStatus(401)
+  const isValid = await authorization.validate(bearer)
   return isValid ? next() : res.sendStatus(403)    
 }
 
 export const authApi = async (req: Request, res: Response, next: NextFunction) => {
-  const authorization = authorizationModule.exists(req)
-  if (authorization == null) return respond.api.auth.fail.authorisationMissing(res)
-  const isValid = await authorizationModule.validate(authorization)
+  const bearer = authorization.exists(req)
+  if (bearer == null) return respond.api.auth.fail.authorisationMissing(res)
+  const isValid = await authorization.validate(bearer)
   return isValid ? next() : respond.api.auth.fail.authorisationInvalid(res)
 }
