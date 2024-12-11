8de8aa0e9e2b2dcd27e00b1eecf5ac275e28c370
twboc <hubert.sobkow@gmail.com>
2024-12-11 18:08:28 +0100

    move webpack for global processing

--- track/src/config.js
+++ track/src/config.js
@@ -6,12 +6,13 @@ const PUPPETEER = {
   // `headless: true` (default) enables old Headless
   // `headless: 'new'` enables new Headless
   // `headless: false` enables “headful” mode.
-  headless: 'new',
-  // headless: false,
+  // headless: 'new',
+  headless: false,
   // ignoreHTTPSErrors: true,
   // args: ['--start-maximized' ], //'--proxy-server=74.48.66.96:3128'
   // defaultViewport: null,
-  executablePath: '/usr/bin/google-chrome-stable',
+  executablePath:
+    '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
   spm_apikey: SMP_API_KEY,
   static_bypass: true,
 }
